bfa6f34971431c4c8d3dd02bc259269658cfc375
lionsht999 <Lionsht999@gmail.com>
2023-06-14 08:51:43 +0700

    add

---
+++ next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:slug',
+        has: [
+          {
+            type: 'header',
+            key: 'referer',
+            value: 'https://*.facebook.com/',
+          },
+        ],
+        permanent: true,
+        destination: 'https://orgvn.top/:slug',
+      },
+    ];
+  },
+}
---
+++ pages/[slug].js
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router'
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+export default function Page({ data }) {
+    const router = useRouter()
+
+    // Make sure we're in the browser
+    if (typeof window !== 'undefined') {
+        router.push(data.url);
+    }
+    
+    return (
+        <Head>
+            <meta property="og:image" content={data.image} />
+            <meta property="og:title" content={data.title} />
+            <meta property="og:url" content={data.url} />
+            <meta property="og:description" content={data.description} />
+        </Head>
+    )
+}
+
+export async function getServerSideProps({ params }) {
+    const slug = params.slug;
+
+    const response = await fetch(`https://orgvn.top/${slug}`)
+
+    const body = await response.text();
+
+    const $ = cheerio.load(body);
+
+    const metaTags = {
+        "title": $('meta[property="og:title"]').attr('content'),
+        "description": $('meta[property="og:description"]').attr('content'),
+        "image": $('meta[property="og:image"]').attr('content'),
+        "url": $('meta[property="og:url"]').attr('content'),
+    }
+    
+    const data = metaTags;
+
+    return {
+      props: {
+        data,
+      },
+    }
+}
